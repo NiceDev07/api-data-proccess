a58ac0f75a7535f288ea650b87a877a5008d773e
estebanDev07 <esteban.nc3010@gmail.com>
2025-06-03 21:44:26 -0500

    Refactor SMSCampaignProcessorUseCase y SMSInformativoProcessor para mejorar la ejecución y el procesamiento de datos, incluyendo validaciones y personalización de mensajes.

--- src/modules/campaign_proccess/application/use_cases/process_sms_campaign.py
+++ src/modules/campaign_proccess/application/use_cases/process_sms_campaign.py
@@ -1,14 +1,11 @@
 from core.file_readers.factory import FileReaderFactory
-
-# Importa los procesadores (o importa en otro módulo y registra)
-# from modules.campaign_proccess.domain.services.sms_informativo_processor import SMSInformativoProcessor
-# from modules.campaign_proccess.domain.services.sms_landing_processor import SMSLandingProcessor
-
+from modules.campaign_proccess.domain.interfaces.proccess import ProcessorInterface
+from modules.campaign_proccess.domain.services.sms.sms_base_proccessor import SMSInformativoProcessor
 
 class SMSCampaignProcessorUseCase:
     # Registro dinámico para subservicios y sus procesadores
     _processor_registry = {
-        # "informativo": SMSInformativoProcessor,
+        "informative": SMSInformativoProcessor,
         # "landing": SMSLandingProcessor,
         # agregar más subservicios aquí
     }
@@ -16,15 +13,25 @@ class SMSCampaignProcessorUseCase:
     def __init__(self):
         pass
 
-    def execute(self, filepath: str, regulation: dict, subservice: str, user_level: int):
+    def execute(self, filepath: str, regulation: dict, user_level: int, subservice: str = "informative"):
         reader = FileReaderFactory.get_reader(filepath)
         nrows = 10 if user_level == 1 else None
         df = reader.read(filepath, usecols=None, nrows=nrows)
-        print(f"Procesando archivo: {filepath} con {len(df)} filas")
-        print(df.head())
-        # processor_cls = self._processor_registry.get(subservice)
-        # if not processor_cls:
-        #     raise ValueError(f"Subservicio '{subservice}' no es soportado")
+        processor_cls = self._processor_registry.get(subservice)
+        if not processor_cls:
+            raise ValueError(f"Subservicio '{subservice}' no es soportado")
 
-        # processor: ProcessorInterface = processor_cls(df, regulation)
-        # return processor.process()
+        processor: ProcessorInterface = processor_cls({})
+        return processor.process(df)
+
+
+use_case = SMSCampaignProcessorUseCase()
+
+df = use_case.execute(
+    filepath="/home/esteban/saem/refactors/api-data-proccess/sms_campaign_file.csv",
+    regulation={"some": "regulation"},
+    user_level=1,
+    subservice="informative"
+)
+
+print(df.head())  # Imprime las primeras filas del DataFrame procesado
--- src/modules/campaign_proccess/domain/services/sms/sms_base_proccessor.py
+++ src/modules/campaign_proccess/domain/services/sms/sms_base_proccessor.py
@@ -1,5 +1,5 @@
 import dask.dataframe as dd
-from src.modules.campaign_proccess.domain.interfaces.proccess import ProcessorInterface
+from modules.campaign_proccess.domain.interfaces.proccess import ProcessorInterface
 
 class SMSInformativoProcessor(ProcessorInterface):
     def __init__(self, regulation: dict):
@@ -7,24 +7,33 @@ class SMSInformativoProcessor(ProcessorInterface):
 
     def process(self, df: dd.DataFrame) -> dd.DataFrame:
         # Aquí la lógica de validación y filtrado con regulation
+        df = self.operator_assignment(df)
+        df = self.prefix_number(df)
+        df = self.customize_message(df)
+        df = self.black_list_check(df)
+
         return df
     
     #Common (CALL BLASTING, SMS)
-    def operator_assignment(df) -> dd.DataFrame:
+    def operator_assignment(self, df) -> dd.DataFrame:
+        df['OPERATOR'] = 'CLARO'
         # Aquí la lógica de asignación de operadores
         return df
     
     #Common (CALL BLASTING, SMS)
-    def prefix_number(df: dd.DataFrame) -> dd.DataFrame:
+    def prefix_number(self, df: dd.DataFrame) -> dd.DataFrame:
+        df['PHONE'] = 0
         # Aquí la lógica de agregar prefijo a los números
         return df
     
     #Common ALL
-    def customize_message(df: dd.DataFrame) -> dd.DataFrame:
+    def customize_message(self, df: dd.DataFrame) -> dd.DataFrame:
         # Aquí la lógica de personalización del mensaje
+        df['MESSAGE'] = 'Mensaje personalizado'
         return df
     
     #Common ALL
-    def black_list_check(df: dd.DataFrame) -> dd.DataFrame:
+    def black_list_check(self, df: dd.DataFrame) -> dd.DataFrame:
         # Aquí la lógica de verificación de listas negras
+        df['BLACK_LIST'] = False
         return df
